5c5b8d4c60ceceea4c2b1c4db21724f7c8a4c058
cbtruong <batruong12122002@gmail.com>
2024-07-01 15:33:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NotFound from './pages/NotFound'
 function App() {
 	return (
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/Intern-Login" element={<Home />} />
 				<Route path="/notfound" element={<NotFound />} />
 			</Routes>
 	);
